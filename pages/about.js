4ec82d3d045d4cc681f1d6ee8e789b880853264f
Jason <jason@google.com>
2020-09-24 18:54:50 -0400

    up

---
+++ pages/about.js
@@ -0,0 +1,28 @@
+import Head from 'next/head'
+import styles from '../styles/Home.module.css'
+
+export default function About() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>About</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className={styles.main}>
+        About something
+      </main>
+
+      <footer className={styles.footer}>
+        <a
+          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Powered by{' '}
+          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
+        </a>
+      </footer>
+    </div>
+  )
+}
